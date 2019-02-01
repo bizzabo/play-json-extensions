8e4d7770a253510dcf3904413d09eb18cfc611cb
João Ferreira <jtjeferreira@gmail.com>
2019-02-01 10:13:16 +0000

    use sizeHint

--- src/main/scala/play-json.scala
+++ src/main/scala/play-json.scala
@@ -56,6 +56,7 @@ package object internals {
     def distinctWith[That]( equivalent: ( A, A ) => Boolean ): Iterable[A] = {
       var l = List[A]()
       val b = Iterable.newBuilder[A]
+      b.sizeHint( coll )
       for ( elem <- coll ) {
         l.find {
           case first => equivalent( elem, first )
